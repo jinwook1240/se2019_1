fd83897dd0594ee56bca6b3fc262c240f888c265
jjw4145 <jjw4145@naver.com>
2019-12-05 23:20:25 +0900

    create member database and create initPage

--- database/mysql.js
+++ database/mysql.js
@@ -1,7 +1,7 @@
 var mysql      = require('mysql');
 // 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다. 
 var connection = mysql.createConnection({
-  host     : '127.0.0.1',
+  host     : 'oreh.onyah.net',
   user     : 'se2019_1',
   password : '00000000',
   database : 'testDB',

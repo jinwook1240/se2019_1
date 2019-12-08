bf95a474658f82f69aab0508142dcfe1794dab50
Jinwook <jinwook1240@gmail.com>
2019-12-08 17:35:41 +0900

    static samplecode

--- database/BusDAO.js
+++ database/BusDAO.js
@@ -1,35 +1,10 @@
-'use strict'
-
-const express = require('express');
-const router = express.Router();
-
-const db_connection = require('./mysql.js');
-db_connection.connect();
-/* GET home page. */
-router.get('/', function(req, res) {
-    db_connection.query('SELECT * FROM team_member', function(query_err, query_res, query_fields){
-        if(query_err){
-            res.send(query_err);
-        }
-        res.render('index', { title: 'Express', member:query_res });
-    });
-});
-class BusDAO {
-    constructor() {
-        this.id = id
-        this.pw = pw
-        this.name = name
-        this.phone = phone
-        this.email = email
-        this.coin = coin
-    }
-
-    signIn() {
-
-    }
-
-    signUp() {
-
-    }
+'use strict';
+if(module.exports.connection == undefined){
+    module.exports.connection = require('../database/mysql.js');
+    module.exports.connection.connect();
 }
-module.exports = router;
+module.exports.somefunction = function(){
+    //쿼리문 넣기
+};
+
+
--- routes/Bus.js
+++ routes/Bus.js
@@ -3,34 +3,8 @@
 const express = require('express');
 const router = express.Router();
 
-const db_connection = require('../database/mysql.js');
-db_connection.connect();
-/* GET home page. */
-class Bus {
-    constructor(id, pw, name, phone, email, coin) {
-        this.id = id
-        this.pw = pw
-        this.name = name
-        this.phone = phone
-        this.email = email
-        this.coin = coin
-    }
-
-    signIn() {
-
-    }
-
-    signUp() {
-
-    }
-}
 router.get('/', function(req, res) {
-    db_connection.query('SELECT * FROM bus', function(query_err, query_res, query_fields){
-        if(query_err){
-            res.send(query_err);
-        }
-        res.render('index', { title: 'Express', bus:query_res });
-    });
+    res.send(require("./statictest"));
 });
 
 module.exports = router;

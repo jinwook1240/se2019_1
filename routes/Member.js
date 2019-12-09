4bc6c396d8abc0f6419b3cc98511ef18a8959157
Joonhee Jeong <jeonggoo75@gmail.com>
2019-12-10 00:24:26 +0900

    Member

--- routes/Member.js
+++ routes/Member.js
@@ -1,5 +1,26 @@
+'use strict';
+
 const express = require('express');
 const router = express.Router();
+const MemberDAO = require("../database/MemberDAO");
+
+router.get('/signIn', (req, res) => { // log in
+    Member.signIn(req.param('id'), req.param('pw'), res);
+});
+
+router.get('/signUp', (req, res) => { // sign up
+    Member.signUp(req.param('id'), req.param('pw'), 
+        req.param('name'), req.param('phone'), req.param('email'), res);
+});
+
+router.get('/signOut', (req, res) => { // log out
+    Member.signOut(res);
+});
+
+router.get('/updateCoin', (req, res) => { // updateCoin
+    Member.updateCoin(req.param('id'), req.param('coin'), res);
+});
+
 
 class Member {
     constructor(id, pw, name, phone, email, coin) {
@@ -11,29 +32,39 @@ class Member {
         this.coin = coin;
     }
 
-    static signIn(id, pw) {
-        MemberDAO.searchMember(id, pw, function(res) {
-            if (res == 0) {
+    static signIn(id, pw, res) {
+        MemberDAO.searchMember(id, pw, (dao_res) => {
+            if (dao_res == 0) {
                 alert('Error');
-            } else if (res == 1) {
+            } else if (dao_res == 1) {
                 alert("Input ID doesn't exist");
-            } else if (res == 2) {
+            } else if (dao_res == 2) {
                 alert("Input password is wrong")
             } else {
-                
+                res.render('SignIn', {'member':dao_res});
             }
         });
     }
 
-    static signUp(id, pw, name, phone, email) {
-        MemberDAO.insertMember(id, pw, function(res) {
-            if (res) alert("Sign Up is completed");
+    static signUp(id, pw, name, phone, email, res) {
+        MemberDAO.insertMember(id, pw, name, phone, email, (dao_res) => {
+            if (dao_res) alert("Sign Up is completed");
             else alert('Error');
         });
     }
 
-    static signOut() {
-        
+    static signOut(res) {
+        res.render('SignOut', {});
+    }
+
+    static updateCoin(id, coin, res) {
+        if (coin < 0) alert('Error');
+        else {
+            MemberDAO.updateCoin(id, coin, (dao_res) => {
+                if (dao_res) res.render('UpdateCoin', {'id' : id, 'coin':coin});
+                else alert('Error');
+            });
+        }
     }
 }
 

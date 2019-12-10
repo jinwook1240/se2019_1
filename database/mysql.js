02bdacb4457475cadefb5c2aa53a162c9b5e6c99
Jinwook <jinwook1240@gmail.com>
2019-12-10 22:08:04 +0900

    Update code about mysql

--- database/mysql.js
+++ database/mysql.js
@@ -1,10 +1,9 @@
-var mysql      = require('mysql');
-// 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다.
+
 if(module.exports.connect === undefined){
-  module.exports = mysql.createConnection({
+  module.exports = require('mysql').createConnection({
     host     : 'oreh.onyah.net',
-    user     : 'se2019_1',
-    password : '00000000',
+    user     : 'root',
+    password : 'orehonyah',
     database : 'jjj',
     port     : '3308'
   });

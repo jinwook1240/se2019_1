ea371f72fadef7df334e46f7abcc7b1368b9684f
Joonhee Jeong <jeonggoo75@gmail.com>
2019-12-12 01:00:10 +0900

    .

--- routes/Reservation.js
+++ routes/Reservation.js
@@ -4,3 +4,11 @@ const express = require('express');
 const router = express.Router();
 const BusDAO = require("../database/BusDAO");
 const SeatRsrvDAO = require("../database/SeatReservationDAO");
+
+router.get('/', (req, res)=> {
+    const condition = 'bus_code="'+req.query.bus_code+'"';
+    console.log(condition);
+    BusDAO.searchBus(condition, (buslist)=>{
+        res.render('Bus', {'buslist':buslist});
+    });
+});
